46d04fec38ecc181ed4d134c48c14b6dc582c520
Jorge Suarez <suarezmjorge@gmail.com>
2026-02-09 16:25:00 +0100

    update contents

---
+++ assets/js/footer.js
@@ -0,0 +1,36 @@
+document.addEventListener("DOMContentLoaded", () => {
+  document.body.insertAdjacentHTML(
+    "beforeend",
+    `
+<footer>
+        <div class="footer-content">
+            <div class="footer-col">
+                <h4>TRUST Project</h4>
+                <!--<p>A research initiative funded by
+                    [INSERTAR funders].</p>-->
+            </div>
+            <div class="footer-col">
+                <h4>Quick Links</h4>
+                <ul class="footer-links">
+                    <li><a href="team.html">Meet the Team</a></li>
+                    <li><a href="news.html">News & Events</a></li>
+                    <li><a href="studies.html">See our studies</a></li>
+                </ul>
+            </div>
+            <div class="footer-col">
+                <h4>Contact</h4>
+                <a href="https://produccioncientifica.ugr.es/investigadores/455064/detalle" target="_blank" rel="noopener" style="color: chocolate;">María Victoria Martínez-López</a><br>
+                <p>Dept. of Nursery<br>University of Granada</p>
+            </div>
+        </div>
+        <div class="footer-bottom">
+            &copy; 2025 TRUST Project.<br>
+            Website by Jorge Suárez 
+            <a href="https://github.com/JorgeSuarezM" target="_blank" rel="noopener">
+                <img src="https://upload.wikimedia.org/wikipedia/commons/2/24/Github_logo_svg.svg" alt="GitHub" width="20" height="20" style="vertical-align: middle;">
+            </a>
+        </div>
+    </footer>
+    `
+  );
+});
